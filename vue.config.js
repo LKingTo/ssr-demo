c703a7cfee0875872e69d57b62352fc23ddac275
leungkingto <jingtao.820@163.com>
2019-07-17 10:54:58 +0800

    修改webpack配置：vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const VueSSRServerPlugin = require("vue-server-renderer/server-plugin");
-const VueSSRClientPlugin = require("vue-server-renderer/client-plugin");
-const nodeExternals = require("webpack-node-externals");
+const VueSSRServerPlugin = require("vue-server-renderer/server-plugin"); //生成服务端清单
+const VueSSRClientPlugin = require("vue-server-renderer/client-plugin"); //生成客户端清单
+const nodeExternals = require("webpack-node-externals"); //忽略node_modules文件夹中的所有模块
 const merge = require("lodash.merge");
 const TARGET_NODE = process.env.WEBPACK_TARGET === "node";
-const target = TARGET_NODE ? "server" : "client";
+const target = TARGET_NODE ? "server" : "client";	//根据环境变量来指向入口
 
 module.exports = {
 	configureWebpack: () => ({
@@ -14,18 +14,20 @@ module.exports = {
 		target: TARGET_NODE ? "node" : "web",
 		node: TARGET_NODE ? undefined : false,
 		output: {
+			// filename: 'js/[name].js',
+			// chunkFilename: 'js/[name].js',
 			libraryTarget: TARGET_NODE ? "commonjs2" : undefined
 		},
 		// https://webpack.js.org/configuration/externals/#function
 		// https://github.com/liady/webpack-node-externals
 		// 外置化应用程序依赖模块。可以使服务器构建速度更快，
 		// 并生成较小的 bundle 文件。
-		externals: nodeExternals({
+		externals: TARGET_NODE ? nodeExternals({
 			// 不要外置化 webpack 需要处理的依赖模块。
 			// 你可以在这里添加更多的文件类型。例如，未处理 *.vue 原始文件，
 			// 你还应该将修改 `global`（例如 polyfill）的依赖模块列入白名单
 			whitelist: [/\.css$/]
-		}),
+		}) : undefined,
 		optimization: {
 			splitChunks: {
 				chunks: "async",
@@ -38,12 +40,24 @@ module.exports = {
 		plugins: [TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()]
 	}),
 	chainWebpack: config => {
+		// config.resolve
+		// 	.alias
+		// 	.set('vue$', 'vue/dist/vue.esm.js');
 		config.module
 			.rule("vue")
 			.use("vue-loader")
 			.tap(options => {
 				merge(options, {
 					optimizeSSR: false
+				});
+			});
+		config.module
+			.rule('images')
+			.use('url-loader')
+			.tap(options => {
+				merge(options, {
+					limit: 1024,
+					fallback:'file-loader?name=img/[path][name].[ext]'
 				});
 			});
 	}
